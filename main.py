eb3c9aabfb482eb046d17854389de9328c52cba6
sapwavino <sapwavino@protonmail.com>
2024-08-14 17:48:37 +0100

    FIX

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fasthtml.common import *
 
-app,rt = fast_app()
+app, rt = fast_app()
 
-@rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
 
-@rt('/change')
-def get(): return P('Nice to be here!')
+@rt("/")
+def get():
+    return Div(P("Hello World!"), hx_get="/change")
+
+
+@rt("/login")
+def get():
+    print("hello")
+    return Div(P("Hello World!"), hx_get="/change")
+
+
+@rt("/change")
+def get():
+    return P("Nice to be here!")
+
 
 serve()
