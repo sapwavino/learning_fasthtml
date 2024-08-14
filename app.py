5f4c2affdc0c0aa95eefc55e191f25d1e26ea4ea
sapwavino <sapwavino@protonmail.com>
2024-08-14 20:35:57 +0100

    NOT WORKING

---
+++ app.py
@@ -0,0 +1,59 @@
+from fasthtml.common import *
+from fasthtml import Card
+from fastcore.xtras import timed_cache
+
+
+_blank = dict(target="_blank", rel="noopener noreferrer")
+description = "Modern web applications in pure Python"
+
+hdrs = [
+    Meta(charset="UTF-8"),
+    Meta(
+        name="viewport",
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0",
+    ),
+    Meta(name="description", content=description),
+    # *Favicon("favicon.ico", "favicon-dark.ico"),
+    # *Socials(
+    #     title="FastHTML",
+    #     description=description,
+    #     site_name="fastht.ml",
+    #     twitter_site="@answerdotai",
+    #     image=f"/assets/og-sq.png",
+    #     url="",
+    # ),
+    Link(href="css/main.css", rel="stylesheet"),
+    Link(href="css/tailwind.css", rel="stylesheet"),
+    # Link(href="css/preview-stack.css", rel="stylesheet"),
+    # Link(href="css/highlighter-theme.css", rel="stylesheet"),
+]
+
+# scripts = [
+#     Script(src="https://cdn.tailwindcss.com"),
+# ]
+
+app = FastHTMLWithLiveReload(hdrs=hdrs)
+
+
+
+def benefit(title, content):
+    return Div(
+        H3(title, cls=f"text-black heading-3"),
+        P(content, cls=f"l-body mt-6 lg:mt-6"),
+        cls="w-full p-6 bg-black rounded-2xl xl:p-12 lg:h-[22rem] lg:w-[26rem] shadow-lg",
+    )
+
+
+# @timed_cache(seconds=60)
+@app.route("/")
+def get():
+    return (
+        Main(
+            Title("Hello"),
+            benefit("A new bensfsefit", "This is asasa new benefit"),
+            # **scripts,
+        ),
+    )
+
+
+serve(reload=True)
---
+++ tailwind.config.js
@@ -0,0 +1,86 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./js/*.js"],
+    theme: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+      extend: {
+        keyframes: {
+          "positive-bounce": {
+            "0%": {
+              transform: "rotate(4deg)",
+            },
+            "33%": {
+              transform: "rotate(3deg)",
+            },
+            "66%": {
+              transform: "rotate(5deg)",
+            },
+            "100%": {
+              transform: "rotate(4deg)",
+            },
+          },
+          "positive-unbounce": {
+            "0%": {
+              transform: "rotate(4deg)",
+            },
+            "100%": {
+              transform: "rotate(0deg)",
+            },
+          },
+          "negative-bounce": {
+            "0%": {
+              transform: "rotate(-4deg)",
+            },
+            "33%": {
+              transform: "rotate(-3deg)",
+            },
+            "66%": {
+              transform: "rotate(-5deg)",
+            },
+            "100%": {
+              transform: "rotate(-4deg)",
+            },
+          },
+          "negative-unbounce": {
+            "0%": {
+              transform: "rotate(-4deg)",
+            },
+            "100%": {
+              transform: "rotate(0deg)",
+            },
+          },
+        },
+        animation: {
+          "positive-bounce": "positive-bounce  0.6s ease-in-out forwards",
+          "positive-unbounce": "positive-unbounce  0.2s ease-in-out forwards",
+          "negative-bounce": "negative-bounce  0.6s ease-in-out forwards",
+          "negative-unbounce": "negative-unbounce  0.2s ease-in-out forwards",
+        },
+        fontFamily: {
+          geist: ["Geist", "sans-serif"],
+          "geist-mono": ["Geist Mono", "monospace"],
+        },
+        colors: {
+          brown: "#6B2E2E",
+          "soft-brown": "#fcc",
+          green: "#3CDD8C",
+          "soft-green": "#D4F7E6",
+          yellow: "#FFC435",
+          "soft-yellow": "#fec",
+          grey: "#F3F3F3",
+          "soft-grey": "#fff",
+          pink: "#E699D9",
+          "soft-pink": "#FFCCF7",
+          purple: "#3A2234",
+          "soft-purple": "#EDDEE9",
+          blue: "#7575F0",
+          "soft-blue": "#E8E8FC",
+        },
+      },
+    },
+  };
